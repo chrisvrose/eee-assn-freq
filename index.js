998cb3804e640520a639bb0b83a1afed2fd13dd0
Christopher Rose <christophervrose@gmail.com>
2019-02-22 13:58:47 +0530

    Introduce basic pages for testing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {app,BrowserWindow}= require('electron');
 
 app.on('ready',()=>{
     let win = new BrowserWindow({show:false});
-    win.loadFile('resources/')
+    win.loadFile('resources/index.html')
+    win.setMenuBarVisibility(false);
     win.on('ready-to-show',()=>{win.show()})
 });
