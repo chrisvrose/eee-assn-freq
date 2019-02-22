ba89cc492fc0751a48b84ade4af697083e31c7b4
Christopher Rose <christophervrose@gmail.com>
2019-02-22 15:17:46 +0530

    Minor updates

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const {app,BrowserWindow}= require('electron');
+const {app,BrowserWindow,ipcMain}= require('electron');
+
+
 
 app.on('ready',()=>{
     let win = new BrowserWindow({show:false})
+    ipcMain.on('ping',(e,args)=>console.log(`pinged ${args}`));
     win.loadFile('resources/index.html')
     win.setMenuBarVisibility(false)
     win.on('ready-to-show',()=>{win.show()})
