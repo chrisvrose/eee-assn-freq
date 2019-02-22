c581021e4ae6fecf0e977b2c3d9fa21642f34f0b
Christopher Rose <christophervrose@gmail.com>
2019-02-22 20:04:34 +0530

    Introduce secondary page

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const {app,BrowserWindow,ipcMain}= require('electron');
 
 app.on('ready',()=>{
     let win = new BrowserWindow({show:false})
-    ipcMain.on('ping',(e,args)=>console.log(`pinged ${args}`));
+    
     win.loadFile('resources/index.html')
     win.setMenuBarVisibility(false)
     win.on('ready-to-show',()=>{win.show()})
-
+     
 });
