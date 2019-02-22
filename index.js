7d20f02f78bd815e11107804c87953356a9fd008
Christopher Rose <christophervrose@gmail.com>
2019-02-22 14:40:09 +0530

    Add all required dependencies

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const {app,BrowserWindow}= require('electron');
 
-
 app.on('ready',()=>{
     let win = new BrowserWindow({show:false})
     win.loadFile('resources/index.html')
     win.setMenuBarVisibility(false)
     win.on('ready-to-show',()=>{win.show()})
+
 });
