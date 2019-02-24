68edc427bd5d9e1d0090b777651f57ab6a4f18fe
Christopher Rose <christophervrose@gmail.com>
2019-02-24 15:25:34 +0530

    knob stuff

--- resources/page.js
+++ resources/page.js
@@ -4,7 +4,11 @@ window.$ = window.jQuery = require('../node_modules/jquery/dist/jquery');
 
 $('document').ready(()=>{
     $(".dial").knob({
+        'min':0,
         'step':1,
+        'max':1000,
+        'width':100,
+        'height':100,
 
         'change': (v)=>{
             document.getElementById('knobText').innerHTML = math.round(v);
