913074705eaf465518778c11b1f4be92582fa6b5
Christopher Rose <christophervrose@gmail.com>
2019-02-28 15:42:14 +0530

    Changes Lib

--- resources/page.js
+++ resources/page.js
@@ -2,6 +2,8 @@ const { ipcRenderer } = require('electron');
 const math = require('mathjs');
 window.$ = window.jQuery = require('../node_modules/jquery/dist/jquery');
 
+let currentState = { V: 230, F: 100, R: 100, L: .001, C: 0.000001 }
+
 $('document').ready(() => {
     $(".dial").knob({
         'min': 0,
@@ -12,13 +14,89 @@ $('document').ready(() => {
         'fgColor': '#5264AE',
 
         'change': (v) => {
-            $("#iF").val(math.round(v));
+            $("#iF").val(math.round(v)).trigger('input',false);
         }
     });
-    $('#iF').on('input', (e) => {
-        $(".dial").val(e.target.value).trigger('change');
+    $('#iF').on('input', (e,b=true) => {
+        $(".dial").val(e.target.value);
+        if(b){
+            $(".dial").trigger('change')
+        }
+        $('#cF').html(e.target.value)
+        updateCurrentSet(parseFloat(e.target.value),5)
     })
+
+    $('#iV').on('input',(e)=>{
+        $('#cV').html(`${e.target.value}`);
+        updateCurrentSet(parseFloat(e.target.value),4)
+    })
+    $('#iR').on('input',(e)=>{
+        $('#cR').html(`${e.target.value}`)
+        updateCurrentSet(parseFloat(e.target.value),1)
+    })
+    $('#iL').on('input',(e)=>{
+        $('#cL').html(`${e.target.value}`)
+        updateCurrentSet(parseFloat(e.target.value),2)
+    })
+    $('#iC').on('input',(e)=>{
+        $('#cC').html(`${e.target.value}`)
+        updateCurrentSet(parseFloat(e.target.value),3)
+    })
+
+
+
+
+    $('#iV').val(230);
+    $('#iR').val(48400);
+    $('#iL').val(0.001);
+    $('#iC').val(.000005);
+    $('#iF').val(50);
+
+    //$().on
+    ['#iV','#iR','#iL','#iC','iF'].forEach(element => {
+        $(element).trigger('input')
+    });
+
+    
+    
+
+
+
 })
+
+
+//1R 2L 3C 4V 5F
+function updateCurrentSet(valueObject, val = 1) {
+    switch (val) {
+        case 1:
+            currentState.R = valueObject
+            break
+        case 2:
+            currentState.L = valueObject
+            break
+        case 3:
+            currentState.C = valueObject
+            break;
+        case 4:
+            currentState.V = valueObject
+            break;
+        case 5:
+            currentState.F = valueObject
+            break;
+        default:
+            return null
+    }
+    calculateResults(currentState)
+    return valueObject;
+}
+
+
+function calculateResults(cState){
+    
+    return null;
+}
+
+
 
 
 //ipcRenderer.send('setGWV',true);
