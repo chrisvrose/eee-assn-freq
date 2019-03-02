e72c9f26d71c0c81e055ff63486146122fa4f5e7
Christopher Rose <christophervrose@gmail.com>
2019-03-02 12:01:55 +0530

    plotly add and usage

--- resources/graphing.js
+++ resources/graphing.js
@@ -1,12 +1,18 @@
 const {ipcRenderer,remote}= require('electron')
 window.$ = window.jQuery = require('../node_modules/jquery/dist/jquery')
+const plot = require('./plotly-latest');
 // Math usage
+//console.log(plot)
 
 const math = require('mathjs')
 // Allows sending and recieving information between pages and main thread
 
 
 $(document).ready(function(){
+    plot.plot( document.getElementById('tester'), [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16] }], {
+        margin: { t: 0 } } );
     $(".titleBarClose").on('click',e=>{
         remote.getCurrentWindow().close()
     })
