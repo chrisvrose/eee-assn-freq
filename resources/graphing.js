a6a6408af83b9869f2563a6ec844a4dc80903991
PapaSmurf2k18 <achinthyas.cs18@rvce.edu.in>
2019-03-03 15:01:17 +0530

    sample sine graph (if only it worked)

--- resources/graphing.js
+++ resources/graphing.js
@@ -20,3 +20,32 @@ $(document).ready(function(){
     })
 })
 //Todo - Recieve graphing data array and plot
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var i;
+for(i=0; i<360; i+= 20){
+ ctx.moveTo(i+5,180);
+    ctx.lineTo(i,180);
+
+}
+ctx.stroke();
+
+var counter = 0, x=0,y=180;
+
+
+//100 iterations
+var increase = 90/180*Math.PI / 9;
+for(i=0; i<=360; i+=10){
+    
+     ctx.moveTo(x,y);
+    x = i;
+    y =  180 - Math.sin(counter) * 120;
+counter += increase;
+     
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    //alert( " x : " + x + " y : " + y + " increase : " + counter ) ;
+
+}
+
